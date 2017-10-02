14f7afafbe8bb8b807fe54144b1b993d840afcfe
sangeetha <sangeetha.detne@gmail.com>
2017-10-01 20:44:28 -0500

    files added
    add all the files

---
+++ sample.js
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+    <head>
+            <meta charset="utf-8">
+            <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">-->
+             <title>Your Choice</title>
+             <meta name="description" content="">
+             <meta name="viewport" content="width=device-width, initial-scale=1">
+             <link rel="apple-touch-icon" href="apple-touch-icon.png">
+     
+             <link rel="stylesheet" href="css/bootstrap.min.css">
+             
+             <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+             <link rel="stylesheet" href="css/main.css">
+             <link rel="stylesheet" href="index.css" type="text/css">
+           
+     
+             <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+         </head>
+         <body>
+             <!--[if lt IE 8]>
+                 <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+             <![endif]-->
+         <nav class="navbar navbar-inverse navbar-fixed-top">
+           <div class="container">
+             <div class="navbar-header">
+             <!--  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">-->
+                 <span class="sr-only">Toggle navigation</span>
+                 <span class="icon-bar"></span>
+                 <span class="icon-bar"></span>
+                 <span class="icon-bar"></span>
+                 <span class="icon-bar"></span>
+               <!--</button>-->
+               <a class="navbar-brand" href="#">Sangeetha Detne</a><br/>
+             </div>
+             <div class="topnav">
+               <a href=""></a>
+               <a href=""></a>
+               <a class="active" href="index.html" target="_blank">Home</a>
+               <a href="YourChoice.html" target="_blank">Your Choice
+               </a>
+               <a href="Contact.html" target="_blank">Contact</a>
+             </div>
+         </div>
+        </nav>
+
+     <link rel="stylesheet" href="Yourchoice.css" type="text/css">
+     <link rel="stylesheet" href="css/bootstrap.min.css">
+    </head>
+        <body>
+            <div class="container1">
+                <div class="row">
+                <h1>BARBEQUE Restaurant</h1>
+            </div>
+        </div>
+            <div class="conatiner2">
+                <div class="row">
+                    <div class="col-md-4"></div>
+                    <div class="col-md-5">
+                     <form action="/action_page.php">
+                     <label>ENTER THE ITEM  :  </label><select name="items">
+                    <option value="Sandwich">Sandwich</option>
+                    <option value="Burger">Burger</option>
+                    <option value="pizza">pizza</option>
+                    <option value="fries">fries</option>
+                  </select><br /><br />
+                   <label>ENTER THE DRINKS  :  </label><select name="drinks">
+                    <option class="opt" value="Coke" >Coke</option>
+                    <option value="LimeDrink">LimeDrink</option>
+                    <option value="Pepsi">Pepsi</option>
+                    <option value="WaterBottle">WaterBottle</option>
+                    </select><br /><br />
+                    <input type="button" name="SUBMIT" value="SUBMIT" id="#btn"/><br /><br />
+                    <label>Pay Now  :  </label><input type="text" name="pay now" value=""/><br /><br />
+                   </form>
+                  </div>  
+                <div class="col-md-3"></div>
+            </div> 
+        `</div>
+        </body>
+</html>
